e7bce98b5130bdfd6240fdc62740392483003904
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2023-01-21 13:17:07 +0000

    added responsive styling for research and about components for tablet (portrait/landscape), phones (portrait/landscape) and also 1080p resolution

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             id: 'schools'
         },
         {
-            name: 'Courses',
+            name: 'Parents & Students',
             address: '/courses',
             id: 'courses'
         },
